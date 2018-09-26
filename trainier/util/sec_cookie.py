397c107e959ed57ad77b395a7c4106a313a5af56
starvii <starvii@gmail.com>
2018-09-26 10:40:34 +0800

    使用chacha20替换rc4

--- trainier/util/sec_cookie.py
+++ trainier/util/sec_cookie.py
@@ -5,45 +5,50 @@
 cookie编码解码
 """
 
-import hmac
+import base64
 import hashlib
+import hmac
+import json
 import secrets
-from . import rc4
+from typing import Dict
+
+from .chacha20 import chacha20_encrypt
 
 
 def enc(secret_key: bytes, clear_data: bytes) -> bytes:
-    token32: bytes = secrets.token_bytes(32)
-    enc1: bytes = rc4.enc(token32, clear_data)
-    h: bytes = hmac.digest(secret_key, token32 + enc1, hashlib.sha256)
-    buf: bytes = token32 + h + enc1
-    enc2: bytes = rc4.enc(secret_key, buf)
-    return enc2
+    iv: bytes = secrets.token_bytes(8)  # 64(8)
+    sk: bytes = hashlib.sha256(secret_key).digest()  # 256(32)
+    c: bytes = chacha20_encrypt(clear_data, sk, iv)
+    h: bytes = hmac.digest(secret_key, c + iv, hashlib.sha256)
+    ret: bytes = iv + h + c
+    return ret
 
 
 def dec(secret_key: bytes, encoded_data: bytes) -> bytes:
-    enc2: bytes = rc4.enc(secret_key, encoded_data)
-    token32: bytes = enc2[:32]
-    h0: bytes = enc2[32:64]
-    enc1: bytes = enc2[64:]
-    h1: bytes = hmac.digest(secret_key, token32 + enc1, hashlib.sha256)
-    if h0 != h1:
-        raise ValueError('hmac failed.')
-    enc0: bytes = rc4.enc(token32, enc1)
-    return enc0
+    iv: bytes = encoded_data[:8]
+    h: bytes = encoded_data[8:40]
+    c: bytes = encoded_data[40:]
+    # verify
+    hv: bytes = hmac.digest(secret_key, c + iv, hashlib.sha256)
+    if h != hv:
+        raise ValueError('HMAC verify failed.')
+    sk: bytes = hashlib.sha256(secret_key).digest()  # 256(32)
+    ret: bytes = chacha20_encrypt(c, sk, iv)
+    return ret
 
 
-# def test():
-#     secret_key = b'ALL IN ONE CISSP'
-#     data = ''' A cookie is stored on a client’s computer in the form of a text file. Its purpose is to remember and track data pertaining to a client’s usage for better visitor experience and site statistics.以及中文测试'''.encode()
-#     x = enc(secret_key, data)
-#     import base64
-#     print(x)
-#     print(base64.urlsafe_b64encode(x))
-#     x[100] = x[100] + 1
-#     d1 = dec(secret_key, x)
-#     print(d1)
-#     print(d1.decode())
-#
-#
-# if __name__ == '__main__':
-#     test()
+class Codec:
+    def __init__(self, secret_key: bytes) -> None:
+        self.secret_key = secret_key
+
+    def enc_dict(self, _dict: Dict) -> str:
+        j: bytes = json.dumps(_dict, separators=(',', ':')).encode()
+        c: bytes = enc(self.secret_key, j)
+        b: bytes = base64.urlsafe_b64encode(c)
+        return b.decode()
+
+    def dec_dict(self, cookie: str) -> Dict:
+        c: bytes = base64.urlsafe_b64decode(cookie.encode())
+        j: bytes = dec(self.secret_key, c)
+        d: Dict = json.loads(j)
+        return d
