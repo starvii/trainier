10beb8af9b853732915dc9d6484c63a3025d4ffc
starvii <starvii@gmail.com>
2018-09-30 00:52:26 +0800

    对option进行美化，如果中英文仅有一个，只显示其中的那个 对trunks进行优化

--- static/js/common.js
+++ static/js/common.js
@@ -1,12 +1,11 @@
 function digest(trunks) {
-    for (let i = 0, len = trunks.length; i < len; i++) {
-        let trunk = trunks[i];
+    for (let trunk of trunks) {
         trunk.en_trunk = trunk.en_trunk.trim();
         trunk.cn_trunk = trunk.cn_trunk.trim();
         trunk.x_trunk = '';
         if (trunk.en_trunk.length > 0 && trunk.cn_trunk.length > 0) {
-            trunk.en_trunk_digest = text_digest(trunk.en_trunk, 50);
-            trunk.cn_trunk_digest = text_digest(trunk.cn_trunk, 50);
+            trunk.en_trunk_digest = textDigest(trunk.en_trunk, 50);
+            trunk.cn_trunk_digest = textDigest(trunk.cn_trunk, 50);
             continue;
         } else if (trunk.en_trunk.length === 0 && trunk.cn_trunk.length === 0) {
             trunk.en_trunk_digest = '';
@@ -17,11 +16,23 @@ function digest(trunks) {
         } else if (trunk.en_trunk.length === 0 && trunk.cn_trunk.length > 0) {
             trunk.x_trunk = trunk.cn_trunk;
         }
-        trunk.x_trunk_digest = text_digest(trunk.x_trunk, 100);
+        trunk.x_trunk_digest = textDigest(trunk.x_trunk, 100);
     }
 }
 
-function text_digest(text, n) {
+function mergeOptions(options) {
+    for (let option of options) {
+        if (option.en_option.length > 0 && option.cn_option.length === 0) {
+            option.x_option = option.en_option;
+        } else if (option.en_option.length === 0 && option.cn_option.length > 0) {
+            option.x_option = option.cn_option;
+        } else if (enOption.length === 0 && cnOption.length === 0) {
+            option.x_option = '';
+        }
+    }
+}
+
+function textDigest(text, n) {
     if (text.length <= n) {
         return text;
     } else {
