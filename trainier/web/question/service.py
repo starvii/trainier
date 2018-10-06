cf13685f41d1d5c00d2ad6ff68f3e6ae61361a4c
starvii <starvii@gmail.com>
2018-10-06 22:29:28 +0800

    树状题目保存功能完成

--- trainier/web/question/service.py
+++ trainier/web/question/service.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import string
 from typing import List, Set
 
 from sqlalchemy.sql import or_
 
-from trainier.dao.model import Trunk, Option, Pic
+from trainier.dao.model import Trunk, Option
 from trainier.dao.orm import Session
 from trainier.util.logger import logger
 from trainier.util.object_id import object_id
@@ -46,9 +46,9 @@ class QuestionService:
                         Option.trunk_id == trunk.entity_id).order_by(
                         Option.order_num.asc()).all()
                     trunk.__setattr__('_options', options)
-                trunk.__dict__['_pics']: List[Pic] = session.query(Pic).filter(
-                    Pic.trunk_id == trunk.entity_id).order_by(
-                    Pic.order_num.asc()).all()
+                # trunk.__dict__['_pics']: List[Pic] = session.query(Pic).filter(
+                #     Pic.trunk_id == trunk.entity_id).order_by(
+                #     Pic.order_num.asc()).all()
             return trunks[0]
         except Exception as e:
             logger.error(e)
@@ -128,10 +128,21 @@ class QuestionService:
             trunk.entity_id = trunk_id
             session.add(trunk)
         else:
-            session.merge(trunk)
+            session.query(Trunk).filter(Trunk.entity_id == trunk.entity_id).update({
+                Trunk.code:trunk.code,
+                Trunk.en_trunk: trunk.en_trunk,
+                Trunk.cn_trunk: trunk.cn_trunk,
+                Trunk.analysis: trunk.analysis,
+                Trunk.source: trunk.source,
+                Trunk.level: trunk.level,
+                Trunk.comment: trunk.comment,
+                Trunk.order_num: trunk.order_num,
+                Trunk.parent: trunk.parent,
+            })
+            # session.merge(trunk)
 
     @staticmethod
-    def __save_options(session: Session, options: List[Option], trunk_id: str):
+    def __save_options(session: Session, options: List[Option], trunk: Trunk):
         """
         trunk新增时，删除所有option后再插入
         trunk修改时，修改已有的option，如果某option在数据库中存在，而在提交中不存在，则删除
@@ -139,7 +150,7 @@ class QuestionService:
         :param options:
         :return:
         """
-        options_db: List[Option] = session.query(Option).filter(Option.trunk_id == trunk_id).all()
+        options_db: List[Option] = session.query(Option).filter(Option.trunk_id == trunk.entity_id).all()
         exist_db_ids: Set[str] = set([i.entity_id for i in options_db])
         write_db_ids: Set[str] = set()
         for option in options:
@@ -147,25 +158,35 @@ class QuestionService:
                 option.entity_id = object_id()
                 session.add(option)
             else:
-                session.merge(option)
+                session.query(Option).filter(Option.entity_id == option.entity_id).update({
+                    Option.trunk_id: option.trunk_id,
+                    Option.code: option.code,
+                    Option.en_option: option.en_option,
+                    Option.cn_option: option.cn_option,
+                    Option.is_true: option.is_true,
+                    Option.order_num: option.order_num,
+                    Option.comment: option.comment,
+                })
+                # session.merge(option)
             write_db_ids.add(option.entity_id)
         delete_db_ids: Set[str] = exist_db_ids - write_db_ids
-        session.query(Option).filter(Option.entity_id.in_(delete_db_ids)).delete()
+        if len(delete_db_ids) > 0:
+            session.query(Option).filter(Option.entity_id.in_(delete_db_ids)).delete()
 
-    @staticmethod
-    def __save_pics(session: Session, pics: List[Pic], trunk_id: str):
-        pics_db: List[Option] = session.query(Pic).filter(Pic.trunk_id == trunk_id).all()
-        exist_db_ids: Set[str] = set([i.entity_id for i in pics_db])
-        write_db_ids: Set[str] = set()
-        for pic in pics:
-            if pic.entity_id is None or len(pic.entity_id.strip()) == 0:
-                pic.entity_id = object_id()
-                session.add(pic)
-            else:
-                session.merge(pic)
-            write_db_ids.add(pic.entity_id)
-        delete_db_ids: Set[str] = exist_db_ids - write_db_ids
-        session.query(Pic).filter(Pic.entity_id.in_(delete_db_ids)).delete()
+    # @staticmethod
+    # def __save_pics(session: Session, pics: List[Pic], trunk_id: str):
+    #     pics_db: List[Option] = session.query(Pic).filter(Pic.trunk_id == trunk_id).all()
+    #     exist_db_ids: Set[str] = set([i.entity_id for i in pics_db])
+    #     write_db_ids: Set[str] = set()
+    #     for pic in pics:
+    #         if pic.entity_id is None or len(pic.entity_id.strip()) == 0:
+    #             pic.entity_id = object_id()
+    #             session.add(pic)
+    #         else:
+    #             session.merge(pic)
+    #         write_db_ids.add(pic.entity_id)
+    #     delete_db_ids: Set[str] = exist_db_ids - write_db_ids
+    #     session.query(Pic).filter(Pic.entity_id.in_(delete_db_ids)).delete()
 
     @staticmethod
     def save(trunk: Trunk) -> None:
@@ -175,16 +196,20 @@ class QuestionService:
             while len(queue) > 0:
                 trunk_cur: Trunk = queue.pop(0)
                 QuestionService.__save_trunk(session, trunk_cur)  # 获取保存动作
-                trunk_children: List[Trunk] = trunk.__dict__.get('_trunks')
-                pics: List[Pic] = trunk_cur.__dict__.get('_pics')
-                QuestionService.__save_pics(session, pics, trunk_cur.entity_id)
+                trunk_children: List[Trunk] = trunk_cur.__dict__.get('_trunks')
+                # pics: List[Pic] = trunk_cur.__dict__.get('_pics')
+                # QuestionService.__save_pics(session, pics, trunk_cur.entity_id)
                 if trunk_children is not None:
                     trunk_cur.parent = ROOT_NODE
-                    for trunk_child in trunk_children:
+                    for idx, trunk_child in enumerate(trunk_children):
                         trunk_child.parent = trunk_cur.entity_id
+                        trunk_child.code = '{}({})'.format(trunk_cur.code, idx + 1)
+                        queue.append(trunk_child)
                 else:  # leaf node
                     options: List[Option] = trunk_cur.__dict__.get('_options')
-                    QuestionService.__save_options(session, options, trunk_cur.entity_id)
+                    for idx, option in enumerate(options):
+                        option.code = '{}-{}'.format(trunk_cur.code, string.ascii_uppercase[idx])
+                    QuestionService.__save_options(session, options, trunk_cur)
             session.commit()
         except Exception as e:
             session.rollback()
