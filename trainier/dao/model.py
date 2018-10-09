7f1a9941808b3570d88d3db6e5ac585d62960bd0
starvii <starvii@gmail.com>
2018-10-09 23:23:43 +0800

    对 order_num 加上索引

--- trainier/dao/model.py
+++ trainier/dao/model.py
@@ -21,7 +21,7 @@ class Trunk(Base):
     source = Column(TEXT, index=True, default='')
     level = Column(Integer, default=0)
     comment = Column(TEXT, default='')
-    order_num = Column(Integer, default=0)
+    order_num = Column(Integer, index=True, default=0)
     parent = Column(String(24), default='')  # 为 '' 时表示单项题目， 'root'时表示根题目节点， 其下的子题目该项为父题目的 ID
 
     def __repr__(self) -> str:
@@ -64,7 +64,7 @@ class Option(Base):
     en_option = Column(TEXT, index=True, default='')
     cn_option = Column(TEXT, index=True, default='')
     is_true = Column(Integer, default=0)
-    order_num = Column(Integer, default=0)
+    order_num = Column(Integer, index=True, default=0)
     comment = Column(TEXT, default='')
 
     def __repr__(self) -> str:
