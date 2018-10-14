d7290abefe454c7b3f51b72943229f4080f4b51a
starvii <starvii@gmail.com>
2018-10-15 00:54:17 +0800

    针对题目嵌套的情况进行修改。 但页面还未同步修改

--- trainier/web/quiz/take/view.py
+++ trainier/web/quiz/take/view.py
@@ -17,7 +17,7 @@ from flask import Blueprint, Response, make_response, abort, render_template, re
 
 from trainier import Config
 from trainier.dao.model import Quiz, Trunk, Option, Result
-from trainier.util.labelify import labelify
+from trainier.util.labelify import labelify, trans_trunk_to_dict
 from trainier.util.logger import logger
 from trainier.util.object_id import object_id
 from trainier.util.sec_cookie import Codec
@@ -33,9 +33,13 @@ trunk_fields = {
     Trunk.entity_id,
     Trunk.code,
     Trunk.en_trunk,
+    Trunk.en_trunk_text,
     Trunk.cn_trunk,
+    Trunk.cn_trunk_text,
+    Trunk.analysis,
     Trunk.source,
     Trunk.level,
+    Trunk.comment,
 }
 
 option_fields = {
@@ -43,21 +47,6 @@ option_fields = {
     Option.en_option,
     Option.cn_option,
 }
-
-# pic_fields = {
-#     Pic.entity_id,
-#     Pic.code,
-#     Pic.name,
-#     Pic.data,
-#     Pic.source,
-#     Pic.order_num,
-#     Pic.comment,
-# }
-
-
-class QuestionVO:
-    # TODO:
-    pass
 
 
 class API:
@@ -115,6 +104,15 @@ class API:
         return quiz_dict
 
     @staticmethod
+    def __multi_choice(options: List[Option]) -> bool or None:
+        c: int = len([option for option in options if option.is_true])
+        if c == 1:
+            return False
+        elif c > 1:
+            return True
+        return None
+
+    @staticmethod
     def __deal_switch(quiz_dict: Dict, switch_to_index: int) -> Dict:
         """
         根据 question_idx 从数据库内读取题目数据
@@ -125,39 +123,29 @@ class API:
         # 读取新的数据
         question: Dict = quiz_dict['questions'][switch_to_index - 1]
         qid: str = question['id']
-        trunk: Trunk = None
-        options: List[Option] = None
-        # pics: List[Pic] = None
-        trunk, options, pics = QuestionService.select_trunks_options_pics_by_id(qid)
-        # 判断是单选还是多选
+        trunk: Trunk = QuestionService.select_trunk_by_id(qid)
         if trunk is None:
             raise ValueError('cannot select trunk by id:' + qid)
-        trunk_dict: Dict = labelify(trunk, trunk_fields)
-        counter: int = len([_ for _ in options if _.is_true])
-        if counter == 1:
-            trunk_dict['multi_choice'] = False
-        elif counter > 1:
-            trunk_dict['multi_choice'] = True
-        options_dict: Dict = labelify(options, option_fields)
+        trunk_dict: Dict = trans_trunk_to_dict(trunk, trunk_fields, option_fields)
+        # 判断是否有子题、以及是单选还是多选
+        sub_trunks: List[Trunk] = trunk.__dict__.get('_trunks')
+        if sub_trunks is None:
+            trunk_dict['multi_choice'] = API.__multi_choice(trunk.__dict__.get('_options'))
+        else:
+            # 对 sub_trunk 处理 option
+            for sub_trunk, sub_trunk_dict in zip(sub_trunks, trunk_dict['trunks']):
+                sub_trunk_dict['multi_choice'] = API.__multi_choice(sub_trunk.__dict__.get('_options'))
+
         # 对选项进行排序
         if quiz_dict.get('random_choice'):
             random.seed(question.get('seed'))
-            random.shuffle(options_dict)
-        # 加上之前保存的答题结果
-        # for option_dict in options_dict:
-        #     option_dict['is_true'] = False
-        # answer: List[str] or str = question['answer']
-        # for ch in answer:
-        #     idx: int = string.ascii_uppercase.find(ch.strip().upper())
-        #     if idx < 0 or idx >= len(answer):
-        #         raise ValueError('something wrong in answer {} of {}'.format(answer, str(question)))
-        #     options_dict[idx]['is_true'] = True
-
-        # pics_dict: Dict = labelify(pics, pic_fields)
+            if sub_trunks is None:
+                random.shuffle(trunk_dict['options'])
+            else:
+                for sub_trunk_dict in trunk_dict['trunks']:
+                    random.shuffle(sub_trunk_dict['options'])
         ret: Dict = dict(
             trunk=trunk_dict,
-            options=options_dict,
-            # pics=pics_dict,
         )
         return ret
 
@@ -175,15 +163,16 @@ class API:
             cookie: str = request.cookies.get('quiz')
             cookie = unquote(cookie)
             quiz_dict: Dict = codec.dec_dict(cookie)
-            if switch_to_index > 0:
-                # 保存提交的结果
-                data: bytes = request.data
-                try:
-                    post_dict: Dict = json.loads(data)
-                    quiz_dict = API.__deal_submit(quiz_dict, post_dict)
-                except json.JSONDecodeError:
-                    # 如果没有提交数据或提交有误，就不进行更新
-                    pass
+
+            # 保存提交的结果
+            data: bytes = request.data
+            try:
+                post_dict: Dict = json.loads(data)
+                quiz_dict = API.__deal_submit(quiz_dict, post_dict)
+            except json.JSONDecodeError as e:
+                # 如果没有提交数据或提交有误，就不进行更新
+                logger.error(str(e))
+            if switch_to_index > 0 and switch_to_index <= len(quiz_dict['questions']):
                 ret: Dict = API.__deal_switch(quiz_dict, switch_to_index)
                 ret['questions'] = API.__list_status(quiz_dict)
             else:
